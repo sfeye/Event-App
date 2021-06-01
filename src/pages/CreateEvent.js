9062792eb931500d8b6bc95bb19de1f1d1b42c70
Samuel Feye <samfeye@gmail.com>
2021-06-01 16:21:06 -0500

    😅 Ok we should be setup now 🤗

---
+++ src/pages/CreateEvent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const CreateEvent = ({navigation}) => {
+    return (
+        <View>
+            <Text>Create Event</Text>
+        </View>
+    )
+}
+
+export default CreateEvent
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,38 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
 import firebase from 'firebase';
 
-class Dashboard extends React.Component {
+const Dashboard = ({route, navigation}) => {
 
-  state = { user: {} };
-  _isMounted = false;
-
-  componentDidMount() {
-    this._isMounted = true;
-
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user != null && this._isMounted) {
-        this.setState({user: user});
-      }
-    })
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  render() {
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={styles.container}>
-          <Text>Welcome to the dashboard {this.state.user.email}!</Text>
-          <Button title="Log Off" onPress={() => {
-            firebase.auth().signOut();
-          }}/>
-        </View>
-      </SafeAreaView>
-    );
-  }
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text>Welcome to the dashboard {route.params.user.email}!</Text>
+        <Button title="Log Off" onPress={() => firebase.auth().signOut()}/>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
