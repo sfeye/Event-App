437597615400490a2aa1d8f22d48e4466dbbf765
Samuel Feye <samfeye@gmail.com>
2021-06-16 07:43:09 -0500

    🌈 color consts  🎨

---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+const primary = "#46B1C9";
+const primary_alt = "#84C0C6";
+
+const secondary = "#9FB7B9";
+const secondary_alt = "#BCC1BA";
+
+const filler = "#F2E2D2";
+const filler_alt = "#FFF";
+
+export { primary, primary_alt, secondary, secondary_alt, filler, filler_alt };
